9a09e3a4770b880d5b52cefc3bdab34a00bd26c2
tanvi05pandey <tanvi05pandey@gmail.com>
2020-06-03 15:05:03 -0700

    Update day15_maxSubarraySumCircular.py

--- day15_maxSubarraySumCircular.py
+++ day15_maxSubarraySumCircular.py
@@ -18,51 +18,7 @@
 # Input: [5,-3,5]
 # Output: 10
 # Explanation: Subarray [5,5] has maximum sum 5 + 5 = 10
-#
 
-
-# EXPLANATION:-
-# EX 1:-
-# A = [1, -2, 3, -2]
-# max = 3   (This means max subarray sum for normal array)
-# min = -2  (This means min subarray sum for normal array)
-# sum = 0   (total array sum)
-# Maximum Sum = 3 (max)
-#
-# EX2:-
-# A = [5, -3, 5]
-# max = 7
-# min  = -3
-# sum = 7
-# maximum subarray sum = 7 - (-3) = 10
-#
-# EX3:-
-# A = [3, -1, 2, -1]
-# max = 4
-# min = -1
-# sum = 3
-# maximum subarray sum  = sum - min = 3 - (-1) = 4
-#
-# EX4:-
-# A = [-2, -3, -1]
-# max = -1
-# min = -6
-# sum = -6
-# In this example, if we do (sum - min) then result is 0. but there is no subarray with sum 0.
-# So, in this case we return max value. that is -1.
-#
-# From above examples,
-# we can understand that
-# maximum sum is either max (we get using kadane's algo) or (sum - min).
-# There is a special case, if sum == min,
-# then maximum sum is max.
-#
-# Let's develop an algorithm to solve this problem.
-# 1. Find maximum subarray sum using kadane's algorithm (max)
-# 2. Find minimum subarray sum using kadane's algorithm (min)
-# 3. Find total sum of the array (sum)
-# 4. Now, if sum == min return max
-# 5. Otherwise, return maximum ( max, sum - min )
 # Time :- O(N)
 # Space :- O(1)
 
